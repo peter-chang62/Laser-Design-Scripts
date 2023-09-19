5144be671640e2f0d58cf99f235f694b0f65fb92
peter chang <pech6593@colorado.edu>
2023-09-19 17:32:17 -0600

    update

--- figure9_disp.py
+++ figure9_disp.py
@@ -9,6 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import clipboard
 from scipy.constants import c
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import colormaps
 
 
 # units
@@ -18,30 +20,91 @@ nm = 1e-9
 km = 1e3
 
 # global variables
-Dg = 26 * ps / (nm * km)  # ps / nmkm -> s / m^2
+Dg = 20 * ps / (nm * km)  # ps / nmkm -> s / m^2
+# Dg = 26 * ps / (nm * km)  # ps / nmkm -> s / m^2
 Dp = 18 * ps / (nm * km)  # ps / nmkm -> s / m^2
 
+ncolors = 256
+color_array = plt.get_cmap("binary")(range(ncolors))
 
-def calc_x_g(x_t, Drt):
+# change alpha values
+color_array[0][-1] = 0  # just send the white values to transparent!
+color_array[-1][-1] = 0.5  # just send the white values to transparent!
+
+# create a colormap object
+map_object = LinearSegmentedColormap.from_list(name="binary_t", colors=color_array)
+
+# register this new colormap with matplotlib
+colormaps.register(cmap=map_object)
+
+
+def calc_x_g_from_fr_D(fr, Drt):
     """
     calculate the amount of gain fiber needed to hit a target round trip
-    dispersion Drt at a target round trip fiber length
+    dispersion Drt at a target repetition rate
 
     Args:
-        x_t (array / float):
-            round trip fiber length
+        fr (array / float):
+            target repetition rate
         Drt (array / float):
             round trip dispersion, doesn't need to be mks because the scale
             divides out
     Returns:
         array / float: gain fiber length(s)
     """
+    x_t = c / 1.5 / fr
     x_p = x_t * (Drt + Dg) / (Dp + Dg)
     x_g = x_t - x_p
     return x_g
 
 
-def calc_x_g_lims(fr_ll_MHz, fr_ul_MHz, D_ll_ps_nmkm, D_ul_ps_nmkm):
+def calc_x_p_from_x_g_D(x_g, Drt):
+    """
+    calculate the passive fiber length needed to hit a target round trip
+    dispersion with a given gain fiber length.
+
+    Typically you know the amount of gain fiber you have, and you would like to
+    know if you have too much or too little passive fiber. So, this calculates
+    the total passive fiber length given the current amount of gain fiber and
+    target round trip dispersion
+
+    Args:
+        x_g (float):
+            length of gain fiber
+        Drt (float):
+            target round trip dispersion
+
+    Returns:
+        float:
+            required total length of passive fiber
+    """
+    term = (Drt + Dg) / (Dp + Dg)
+    return x_g * term / (1 - term)
+
+
+def calc_d_from_fr_x_g(fr, x_g):
+    """
+    calculate the round trip dispersion given the current repetition rate and
+    length of gain fiber. This is again convenient to have when splicing,
+    because the quantities easier to measure are the current length of gain
+    fiber, and the measured rep-rate
+
+    Args:
+        fr (float):
+            repetition rate
+        x_g (float):
+            length of gain fiber in the loop
+
+    Returns:
+        float:
+            calculated round trip dispersion
+    """
+    x_t = c / 1.5 / fr
+    x_p = x_t - x_g
+    return (x_p * Dp - x_g * Dg) / x_t
+
+
+def calc_x_g_lims_from_fr_D(fr_ll_MHz, fr_ul_MHz, D_ll_ps_nmkm, D_ul_ps_nmkm):
     """
     calculate the gain fiber length limits needed to map out the paramter space
     fr: ll -> ul, and Drt: ll -> ul
@@ -67,7 +130,7 @@ def calc_x_g_lims(fr_ll_MHz, fr_ul_MHz, D_ll_ps_nmkm, D_ul_ps_nmkm):
     fr = np.linspace(fr_ll_MHz, fr_ul_MHz, 500) * 1e6
     D = np.linspace(D_ll_ps_nmkm, D_ul_ps_nmkm, 500) * ps / nm / km
     fr_2D, D_2D = np.meshgrid(fr, D)
-    x_g_2D = calc_x_g(c / 1.5 / fr_2D, D_2D)
+    x_g_2D = calc_x_g_from_fr_D(fr_2D, D_2D)
     return x_g_2D.min(), x_g_2D.max()
 
 
@@ -87,7 +150,7 @@ def params(fr_ll_MHz, fr_ul_MHz, D_ll_ps_nmkm, D_ul_ps_nmkm, plot=False):
     """
 
     # calculate the limits of gain fiber lengths in play
-    x_g_ll, x_g_ul = calc_x_g_lims(
+    x_g_ll, x_g_ul = calc_x_g_lims_from_fr_D(
         fr_ll_MHz,
         fr_ul_MHz,
         D_ll_ps_nmkm,
@@ -139,10 +202,11 @@ def params(fr_ll_MHz, fr_ul_MHz, D_ll_ps_nmkm, D_ul_ps_nmkm, plot=False):
 
 
 # %% --------------------------------------------------------------------------
-fr_ll_MHz = 125
-fr_ul_MHz = 200
-D_ll_ps_nmkm = 0.5
-D_ul_ps_nmkm = 1.5
+fr_ll_MHz = 100 + 15
+fr_ul_MHz = 100 - 15
+fr_target = 100 * 1e6
+D_ll_ps_nmkm = 2.5
+D_ul_ps_nmkm = 4.0
 x_p, x_g, fr, D, mask = params(
     fr_ll_MHz,
     fr_ul_MHz,
@@ -150,8 +214,34 @@ x_p, x_g, fr, D, mask = params(
     D_ul_ps_nmkm,
 )
 
+fr_masked = fr.copy()
+fr_masked[mask] = np.nan
+
+D_masked = D.copy()
+D_masked[mask] = np.nan
+
+target_fr_window = np.zeros(fr.shape)
+width = 0.5e6
+mask_2 = (abs(fr - fr_target) < width).nonzero()
+not_mask_2 = (abs(fr - fr_target) >= width).nonzero()
+target_fr_window[mask_2] = 1
+target_fr_window[not_mask_2] = 0
+
+# ----- D entire parameter space
+# fig, ax = plt.subplots(1, 1, figsize=np.array([5.25, 3.85]))
+# img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, D / (ps / nm / km), cmap="jet")
+# ax.set_xlabel("pigtail lengths (cm)")
+# ax.set_ylabel("gain fiber length (cm)")
+# plt.colorbar(img, label="$\\mathrm{D_{RT}}$ (ps/nmkm)")
+# fig.suptitle(
+#     f"round trip dispersion for $f_r$ b/w {fr_ll_MHz} MHz and {fr_ul_MHz } MHz"
+# )
+# fig.tight_layout()
+
+# ----- D only valid parameter space
 fig, ax = plt.subplots(1, 1, figsize=np.array([5.25, 3.85]))
-img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, D / (ps / nm / km), cmap="jet")
+img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, D_masked / (ps / nm / km), cmap="jet")
+img_ = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, target_fr_window * 1e-6, cmap="binary_t")
 ax.set_xlabel("pigtail lengths (cm)")
 ax.set_ylabel("gain fiber length (cm)")
 plt.colorbar(img, label="$\\mathrm{D_{RT}}$ (ps/nmkm)")
@@ -160,18 +250,20 @@ fig.suptitle(
 )
 fig.tight_layout()
 
-fig, ax = plt.subplots(1, 1, figsize=np.array([5.25, 3.85]))
-img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, fr * 1e-6, cmap="jet")
-ax.set_xlabel("pigtail lengths (cm)")
-ax.set_ylabel("gain fiber length (cm)")
-plt.colorbar(img, label="repetition rate (MHz)")
-fig.suptitle(f"valid fiber lengths for $f_r$ b/w {fr_ll_MHz} MHz and {fr_ul_MHz } MHz")
-fig.tight_layout()
+# ----- fr entire parameter space
+# fig, ax = plt.subplots(1, 1, figsize=np.array([5.25, 3.85]))
+# img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, fr * 1e-6, cmap="jet")
+# # img_ = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, target_fr_window * 1e-6, cmap="binary_t")
+# ax.set_xlabel("pigtail lengths (cm)")
+# ax.set_ylabel("gain fiber length (cm)")
+# plt.colorbar(img, label="repetition rate (MHz)")
+# fig.suptitle(f"valid fiber lengths for $f_r$ b/w {fr_ll_MHz} MHz and {fr_ul_MHz } MHz")
+# fig.tight_layout()
 
-fr_masked = fr.copy()
-fr_masked[mask] = np.nan
+# ----- fr only valid parameter space
 fig, ax = plt.subplots(1, 1, figsize=np.array([5.25, 3.85]))
 img = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, fr_masked * 1e-6, cmap="jet")
+img_ = ax.pcolormesh(x_p * 1e2 / 8, x_g * 1e2, target_fr_window * 1e-6, cmap="binary_t")
 ax.set_xlabel("pigtail lengths (cm)")
 ax.set_ylabel("gain fiber length (cm)")
 plt.colorbar(img, label="repetition rate (MHz)")
