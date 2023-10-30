156cc371a2df4bcbe02a9c59c969fb62ae68aafc
peter chang <pech6593@colorado.edu>
2023-10-30 13:44:03 -0600

    deleted a bunch of files for clean start of develop branch

--- parse_mathematica.py
+++
@@ -1,9 +0,0 @@
-from sympy.parsing.mathematica import parse_mathematica
-
-file = open("parse_mathematica.txt", "r")
-n1 = parse_mathematica(file.readline().split("\n")[0])
-n2 = parse_mathematica(file.readline().split("\n")[0])
-n3 = parse_mathematica(file.readline().split("\n")[0])
-n4 = parse_mathematica(file.readline().split("\n")[0])
-n5 = parse_mathematica(file.readline().split("\n")[0])
-file.close()
